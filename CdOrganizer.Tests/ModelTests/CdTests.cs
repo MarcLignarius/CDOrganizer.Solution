f505493e01d8fd9f691dcd0fc9a7e03426d88569
Joe Barnes and Marc Davies <joe@>
2019-05-02 14:37:16 -0700

    fail and pass test for SetTitle_SetTitle_String()

--- CdOrganizer.Tests/ModelTests/CdTests.cs
+++ CdOrganizer.Tests/ModelTests/CdTests.cs
@@ -34,5 +34,22 @@ namespace CdOrganizer.Tests
           Assert.AreEqual(title, result); //Makes this test pass
       }
 
+      [TestMethod]
+      public void SetTitle_SetTitle_String()
+      {
+          //Arrange
+          string title = "Rust In Peace";
+          CD newCd = new CD(title);
+
+          //Act
+          string updatedTitle = "Master Of Puppets";
+          newCd.SetTitle(updatedTitle);
+          string result = newCd.GetTitle();
+
+          //Assert
+          Assert.AreEqual(updatedTitle, result);
+          // Assert.AreEqual(title, result); //Makes this test fail
+      }
+
   }
 }
